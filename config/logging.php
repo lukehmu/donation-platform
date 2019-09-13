8fa938f14ad6c255a7896ea3b2d64aabc4afcebb
Luke <lukehmu@gmail.com>
2019-09-13 20:19:30 +0100

    support heroku logging and building via npm

--- config/logging.php
+++ config/logging.php
@@ -41,8 +41,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
+            // 'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
